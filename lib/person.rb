411b9462876644fe4d664069959d1de1f6e51f41
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-08-22 12:45:33 +0300

    commit tree b6400214a3e3a840523db50ed5b98f5bb10c5adc

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # your code goes here
+class Person
+  attr_reader :name
+  # attr_reader :bank_account
+  attr_accessor :happiness
+  attr_accessor :hygiene
+  def initialize(name)
+    @name = name
+  end
+
+  def bank_account=(account_balance="$25")
+   @bank_account = account_balance
+  end
+
+  def bank_account
+   pp @bank_account
+  end
+end
